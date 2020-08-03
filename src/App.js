5e934661c3b401f2eab2bb86039416e50b85d8d6
“ericelvendahl” <ericelvendahl@gmail.com>
2020-08-02 20:22:08 -0500

    list working and displaying stringified data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-//import "./App.css";
+import "./App.css";
 import List from "./Components/List/List";
 import List2 from "./Components/List2/List2";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <List></List>
-        <List2></List2>
+        {/* <List></List> */}
+        <p>A sortable list of cool things:</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.dd
+          <List2></List2>
         </p>
       </header>
     </div>
